31a09a773373602189f922fca9f7fb01dce0b86a
lucas oliveira <locbeso@gmail.com>
2026-09-07 21:25:10 -0300

    feat:Add testes de divisão, produto, soma e subtração

---
+++ atv2/operações/soma/A+B.py
@@ -0,0 +1,28 @@
+from time import time
+import matplotlib.pyplot as plt
+import numpy as np
+
+r=30
+n=100000
+tau_A = []
+a=3
+b=5
+for r in range(r):
+    t = 0
+    for i in range(n):
+        tic = time()
+        a+b
+        toc = time()
+        t+=toc - tic
+    tau_A.append(t/n)
+
+#controle
+media = np.mean(tau_A)
+Sigma = np.std(tau_A)
+cv = Sigma/media
+
+# if cv < 0.15 : ok!
+
+print(f"Média do tempo por operação: {media:.10f} s")
+print(f"Desvio Padrão (Sigma): {Sigma:.10f}")
+print(f"Coeficiente de Variação (CV): {cv:.4f}")
